47e69f79c0e3d4f190a071f3c983ca10914f4f3b
Bertie Blackman <blackmanrgh@gmail.com>
2017-09-03 21:09:49 +0100

    Writing to files

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-const apiDetails = require('./api.js')
+const fs = require('fs')
 const Client = require('coinbase').Client
+const apiDetails = require('./api.js')
+
 const client = new Client(apiDetails)
 
-client.getBuyPrice({ 'currencyPair': 'LTC-USD' }, function (err, price) {
-  console.log(price)
-  console.log(err)
-})
+function writeTofile (whichFile, ApiError) {
+  fs.appendFile(whichFile, ApiError, function (err) {
+    if (err) {
+      return console.log(err)
+    }
+  })
+}
+
+function checkPrices () {
+  client.getSpotPrice({ 'currencyPair': 'LTC-USD' }, function (err, price) {
+    if (err === null) {
+      writeTofile('./output/prices.log', price.data.amount + '\n')
+    }
+    if (err !== null) {
+      writeTofile('./output/errors.log', err + '\n')
+    }
+  })
+}
+
+setInterval(checkPrices, 3 * 1000)
